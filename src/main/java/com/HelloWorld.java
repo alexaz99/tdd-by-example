912159a0cd59b1fc264111b2e634fae3956bd9d3
Alex <azakotyansky@gmail.com>
2021-11-24 09:42:35 -0500

    Added first Junit test.

---
+++ src/main/java/com/HelloWorld.java
@@ -0,0 +1,14 @@
+package com;
+
+public class HelloWorld {
+
+    private String hello;
+
+    public void setHello(String aVal) {
+        this.hello = aVal;
+    }
+
+    public String hello() {
+        return hello;
+    }
+}
