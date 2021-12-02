21c36213366ec1d9a1f754be62c5a83e55ece64a
Alex <azakotyansky@gmail.com>
2021-12-02 15:27:39 -0500

    Added first Junit test.

--- src/main/java/guru/springframework/Sum.java
+++ src/main/java/guru/springframework/Sum.java
@@ -5,7 +5,7 @@ public class Sum implements Expression {
     Money augmend;
     Money addmend;
 
-    public Sum(Money augment, Money addmend) {
+    public Sum(Money augmend, Money addmend) {
         this.augmend = augmend;
         this.addmend = addmend;
     }
